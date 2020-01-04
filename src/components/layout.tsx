09f8c946080446d2ebaef5e446685b923918864b
Lukas <fischi.ftw@gmail.com>
2020-01-04 18:05:14 +0100

    renamed header to Header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import { graphql, useStaticQuery } from "gatsby"
 import React, { FC } from "react"
-import Header from "./header"
+import Header from "./Header"
 import "./layout.scss"
 
 const Layout: FC<{}> = ({ children }) => {
