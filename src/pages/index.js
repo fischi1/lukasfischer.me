da5a7326469153c37f40491c1126518045f13182
Lukas <fischi.ftw@gmail.com>
2020-01-04 16:05:36 +0100

    added query for testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,13 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
     <Layout>
         <SEO title="Home" />
         <h1>Hi people</h1>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
+        <p><i>{data.site.siteMetadata.description}</i></p>
         <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
             <Image />
         </div>
@@ -19,3 +20,13 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+    query {
+        site {
+            siteMetadata {
+                description
+            }
+        }
+    }
+`
