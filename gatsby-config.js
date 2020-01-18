0a5d37a7c947bbbe6e6aa302c1f7ee5241576942
Lukas <fischi.ftw@gmail.com>
2020-01-18 16:57:56 +0100

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
                 background_color: `#040507`,
                 theme_color: `#040507`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+                icon: `src/images/favicon.png` // This path is relative to the root of the site.
             }
         },
         `gatsby-plugin-typescript`,
