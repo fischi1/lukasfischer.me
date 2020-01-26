7451a594ddca107f9cea081ddff4f4966956bff4
Lukas <fischi.ftw@gmail.com>
2020-01-26 14:52:22 +0100

    portrait styling

--- src/components/Portrait.tsx
+++ src/components/Portrait.tsx
@@ -1,13 +1,26 @@
+import { graphql, useStaticQuery } from "gatsby"
+import GatsbyImage from "gatsby-image"
 import React, { FC } from "react"
-import Image from "../components/Image"
 import "./Portrait.scss"
 
 type Props = {}
 
 const Portrait: FC<Props> = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            portrait: file(relativePath: { eq: "images/fischi.png" }) {
+                childImageSharp {
+                    fluid(maxWidth: 1080, quality: 90, base64Width: 50) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+        }
+    `)
+
     return (
-        <div className="portrait-component mr-0 mr-md-5">
-            <Image imageName="portrait" />
+        <div className="portrait-component mr-0 mr-md-4 mb-3 mb-md-0">
+            <GatsbyImage fluid={data.portrait.childImageSharp.fluid} />
         </div>
     )
 }
