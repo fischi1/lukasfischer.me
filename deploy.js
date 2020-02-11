7054109d6b453020d8dbe38ade0bdd98179770be
Lukas <fischi.ftw@gmail.com>
2020-02-11 23:34:08 +0100

    automated ghpages deployment

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const ghpages = require("gh-pages")
+const packagejson = require("./package.json")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+if (!process.env.DEPLOY_REPOSITORY || !process.env.DEPLOY_BRANCH) {
+    console.error(
+        "ERROR! env vars DEPLOY_REPOSITORY and DEPLOY_BRANCH have to be set"
+    )
+    process.exit(1)
+}
+
+ghpages.publish(
+    "public",
+    {
+        message: `Publishing v${packagejson.version}`,
+        repo: process.env.DEPLOY_REPOSITORY,
+        branch: process.env.DEPLOY_BRANCH
+    },
+    function(err) {
+        if (err)
+            console.error(
+                `ERROR trying to publish v${packagejson.version}`,
+                err
+            )
+        else console.log(`Published v${packagejson.version}`)
+    }
+)
