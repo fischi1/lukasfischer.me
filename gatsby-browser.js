cefdbebc50ac3d1c8ba26685c05ea996d6d961ed
Lukas <fischi.ftw@gmail.com>
2020-02-12 22:23:00 +0100

    hint to the repository in the console

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,20 @@ export const onClientEntry = () => {
         import(`intersection-observer`)
         console.log(`# IntersectionObserver is polyfilled!`)
     }
+
+    const styles = [
+        "color: #ffc914;",
+        "background:#040507;",
+        "font-size:25px;",
+        "padding: 10px 15px;",
+        "display: block;",
+        "font-family: monospace"
+    ].join(";")
+    
+    console.log("%cHello 👋😃", styles)
+    console.log("%cInterested in the source code? ⚛️", styles)
+    console.log(
+        "%cCheck it out on github https://github.com/fischi1/lukasfischer.me",
+        styles
+    )
 }
