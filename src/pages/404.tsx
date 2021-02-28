81a64d648f1cd320964816354f74d29a4ad75ee3
Lukas <fischi.ftw@gmail.com>
2021-02-28 16:58:07 +0100

    customized 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,16 @@
 import { PageProps } from "gatsby"
 import React, { FC } from "react"
+import Layout from "../components/layout/Layout"
+import HeaderPlaceholder from "../components/landing/HeaderPlaceholder"
 
 const NotFoundPage: FC<PageProps> = () => (
-    <>
-        <h1>404: Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </>
+    <Layout>
+        <HeaderPlaceholder />
+        <h1 className="text-center mt-5 h3">404: Not Found</h1>
+        <p className="text-center">
+            You just hit a route that doesn&#39;t exist
+        </p>
+    </Layout>
 )
 
 export default NotFoundPage
